a03083169afdb162b045352690e1049c0dfc4612
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-04-09 12:47:10 +0200

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from reina.reinas import JuegoReinas
-#from caballo.caballos import ProblemaCaballo
+from caballo.caballos import ProblemaCaballo
 from hanoi.TorresDeHanoi import ProblemaHanoi
 
 if __name__ == "__main__":
     JuegoReinas
+    ProblemaCaballo
     ProblemaHanoi
+
